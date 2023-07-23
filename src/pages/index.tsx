8d641b8dffdb89e511424910383bbe44a7117455
Curtis Babin <curtsocal@gmail.com>
2023-07-22 18:39:11 -0700

    Add tree + set dev host for mobile testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Tree from 'public/tree.svg';
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
@@ -12,7 +13,7 @@ export default function Home() {
         <title>Welcome :)</title>
         <meta name="description" content="Curtis Babin: Front-End Developer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/tree.svg" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
 
@@ -20,6 +21,12 @@ export default function Home() {
           <h1>Curtis Babin</h1>
           <h2>Professional Front-End Developer</h2>
         </header>
+        <Image 
+          src="/tree.svg"
+          width={400}
+          height={400}
+          alt="Blue tree"
+        />
       </main>
     </>
   )
