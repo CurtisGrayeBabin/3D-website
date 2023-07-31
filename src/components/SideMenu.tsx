cb21621f28d564e1f66a5427d2398bdb2e2e9cc2
Curtis Babin <curtsocal@gmail.com>
2023-07-30 18:16:49 -0700

    Add graceful slide-in slide-out to side menu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -5,24 +5,29 @@ const SideMenu = () => {
 
     const [open, openOrClose] = useState(false);
 
+    const toggleMenu = () => {
+        openOrClose(open => !open);
+      };
+
     return (
         <div className={`${styles.sideMenu}`} aria-live="polite">
             <label 
                 htmlFor="menu-toggle" 
                 className={`${styles.menuIcon}`}
-                onClick={() => openOrClose(open => !open)}
+                onClick={() => toggleMenu()}
                 >{!open ? <span>&#9776;</span> : <span>&#10005;</span>}</label>
 
-            {open ? 
-                <nav id="sideMenuLinks">
-                    <a href="/About" target="_">About</a>
-                    <a href="/Blog" target="_">Blog</a>
-                    <a href="https://github.com/CurtisGrayeBabin" target="_">GitHub</a>
-                    <a href="https://likedin.com/in/curtisbabin/" target="_">LinkedIn</a>
-                    <a href="/Music" target="_">Music</a>
-                    <a href="/Works" target="_">Works</a>
-                </nav>
-            : ''}
+            {/* gracefull slide-in */}
+            <nav className={open ? `${styles.open}` : `${styles.close}`}>
+                <ul>
+                    <li><a href="/About" target="_">About</a></li>
+                    <li><a href="/Blog" target="_">Blog</a></li>
+                    <li><a href="https://github.com/CurtisGrayeBabin" target="_">GitHub</a></li>
+                    <li><a href="https://linkedin.com/in/curtisbabin/" target="_">LinkedIn</a></li>
+                    <li><a href="/Music" target="_">Music</a></li>
+                    <li><a href="/Works" target="_">Works</a></li>
+                </ul>
+            </nav>
         </div>
     );
 }
