af7bd3ac545e5359041678110de5a929cb1be099
Curtis Babin <curtsocal@gmail.com>
2023-08-06 10:20:19 -0700

    Add rel noopener noreferrer to privacy policy link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from '@/styles/Footer.module.css';
 const Footer = () => {
     return (
         <div className={`${styles.footer} bottomLeft`}>
-            <a href="https://www.termsfeed.com/live/3fbd4032-b635-4816-a90d-ed310ec9d468" target="_">Privacy Policy</a>
+            <a href="https://www.termsfeed.com/live/3fbd4032-b635-4816-a90d-ed310ec9d468" target="_" rel="noopener noreferrer">Privacy Policy</a>
         </div>
     );
 }
