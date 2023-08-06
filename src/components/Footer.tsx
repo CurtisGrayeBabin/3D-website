f2c62158510f4a06f0e35f9c3ce946211a7ca3ec
Curtis Babin <curtsocal@gmail.com>
2023-08-06 10:39:01 -0700

    Add aria-label to footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from '@/styles/Footer.module.css';
 const Footer = () => {
     return (
         <div className={`${styles.footer} bottomLeft`}>
-            <a href="https://www.termsfeed.com/live/3fbd4032-b635-4816-a90d-ed310ec9d468" target="_" rel="noopener noreferrer">Privacy Policy</a>
+            <a href="https://www.termsfeed.com/live/3fbd4032-b635-4816-a90d-ed310ec9d468" target="_" rel="noopener noreferrer" aria-label="Website privacy policy page - link opens in a new tab">Privacy Policy</a>
         </div>
     );
 }
