e0779e8bcfff87d90e107b0097400c262384693f
Curtis Babin <curtsocal@gmail.com>
2023-09-24 18:31:01 -0700

    Add Custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,23 @@
+import PageLayout from '@/components/PageLayout';
+import Link from 'next/link';
+import styles from '@/styles/Page.module.css';
+
+export default function About() {
+
+  // simpler way of disallowing Y scrollbar on index page
+  if (typeof window !== 'undefined') {
+    document.documentElement.style.overflowY = 'hidden';
+  }
+
+  return (
+    <PageLayout pageTitle="Curtis Babin | About" pageDescription="About Curtis Babin">
+
+        <div className={`${styles.center}`}>
+            <h1>404</h1> 
+            <h2>Not Found!</h2>
+            <Link href="/">Go Home</Link>
+        </div>
+
+    </PageLayout>
+  )
+}
