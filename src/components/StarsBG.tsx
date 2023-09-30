c4cbac1f43ee2f0d3c692f4fad7e9aa6d0bd7e50
Curtis Babin <curtsocal@gmail.com>
2023-09-29 17:10:54 -0700

    Set star count 10k to 1k

--- src/components/StarsBG.tsx
+++ src/components/StarsBG.tsx
@@ -21,7 +21,7 @@ const RotatingStars = () => {
         <Stars 
           radius={100} 
           depth={100} 
-          count={10000} 
+          count={1000} 
           factor={10} 
           saturation={100} 
           fade speed={.4} 
