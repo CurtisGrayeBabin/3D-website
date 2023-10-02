13566dfad4c2489339f2b7bdde0e81e229dac4a5
Curtis Babin <curtsocal@gmail.com>
2023-10-02 10:30:36 -0700

    Add meta tags to PageLayout

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -13,6 +13,9 @@ const PageLayout: React.FC<PageLayoutProps> = ({ pageTitle, pageDescription, chi
     <>
       <Head>
         <title>{pageTitle}</title>
+        <meta property="og:site_name" content="Curtis Babin" />
+        <meta property="og:type" content="website" />
+        <meta name="theme-color" content="#000" />
         <meta name="description" content={ pageDescription } />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/shuttle.svg" />
